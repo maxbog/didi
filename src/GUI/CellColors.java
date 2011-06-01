da57ff2de76781f20d48a005752688108b2939a2
Damian Sidor <dsidor@gmail.com>
2011-06-01 12:05:45 +0200

    Poprawka kolo nieskonczonosci.

--- src/GUI/CellColors.java
+++ src/GUI/CellColors.java
@@ -2,7 +2,6 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package GUI;
 
 import java.awt.Color;
@@ -18,23 +17,32 @@ public class CellColors {
      * @param number
      * @return color connected to number
      */
-    public static Color getMapColor(int number){
-        switch (number){
-            case 0: return Color.WHITE;
-            case -1: return Color.BLACK;
-            case -2: return Color.GREEN;
-            case -4: return Color.DARK_GRAY;
-            case 1: return Color.RED;
-            default: return Color.YELLOW;
+    public static Color getMapColor(int number) {
+        switch (number) {
+            case 0:
+                return Color.WHITE;
+            case -1:
+                return Color.BLACK;
+            case -2:
+                return Color.GREEN;
+            case -4:
+                return Color.DARK_GRAY;
+            case 1:
+                return Color.RED;
+            default:
+                return Color.YELLOW;
         }
     }
 
-    public static Color getPotentialColor(int number, int maxValue){
-        float index = ((float)number)/((float)maxValue);
-        return Color.getHSBColor((1-(float)index)*2f/3f, 0.9f, 1f);
+    public static Color getPotentialColor(int number, int maxValue) {
+        float index = ((float) number) / ((float) maxValue);
+        if (index > 1f) {
+            index = 1f;
+        }
+            return Color.getHSBColor((1 - (float) index) * 2f / 3f, 0.9f, 1f);
     }
 
-    public static Color getDensityColor(double number){
-        return Color.getHSBColor((1-(float)number)*2f/3f, 0.9f, 1f);
+    public static Color getDensityColor(double number) {
+        return Color.getHSBColor((1 - (float) number) * 2f / 3f, 0.9f, 1f);
     }
 }
