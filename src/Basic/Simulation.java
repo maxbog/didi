9848b824e4e3a877811792d756cb4b5baa8d9dbf
Mirel <mirelka.jkw@gmail.com>
2011-06-03 19:06:19 +0200

    reguła przejścia dla paniki = 3

--- src/Basic/Simulation.java
+++ src/Basic/Simulation.java
@@ -97,6 +97,83 @@ public class Simulation {
         newPosition = equalMinPos.get(random.nextInt(equalMinPos.size()));
         return newPosition;
     }
+    
+     /**
+     * Regula przejscia dla człowieka mocno spanikowanego. Zwraca nowa pozycje lub taka sama, jesli nie ma mozliwosci wykonania ruchu
+     * @param row
+     * @param column
+     * @return nastepna pozycja, ktora zajmuje człowiek
+     */
+ 
+    public Position transitionRule3(int row, int column) {
+
+        int pot, tempPot, id = -1;
+        int exitId[] = new int[2];
+        for (int i = 0; i < 2; ++i) {
+            pot = Integer.MAX_VALUE;
+            for (int j = 1; j <= simGrid.getExitsCount(); ++j) {
+                tempPot = simGrid.getPotential(row, column)[j];
+                if (tempPot < pot && j != id) {
+                    pot = tempPot;
+                    exitId[i] = j;
+                    id = j;
+                }
+            }
+        }
+
+        if (simGrid.getAverageExitDens(exitId[0]) <= simGrid.getAverageExitDens(exitId[1])) {
+            id = exitId[0];
+        } else {
+            id = exitId[1];
+        }
+
+        Position[] Positions = new Position[8];
+        Positions[0] = new Position(row + 1, column - 1); // leftTop
+        Positions[1] = new Position(row + 1, column); // middleTop
+        Positions[2] = new Position(row + 1, column + 1); // rightTop
+        Positions[3] = new Position(row, column - 1); //leftMiddle
+        Positions[4] = new Position(row, column + 1); //rightMiddle
+        Positions[5] = new Position(row - 1, column - 1); //leftBottom
+        Positions[6] = new Position(row - 1, column); //middleBottom
+        Positions[7] = new Position(row - 1, column + 1); //rightBottom
+
+        int minPot = Integer.MAX_VALUE;
+        int minCellPot[] = new int[8];
+        
+        for (int i = 0; i < 8; ++i) {
+            if ((Positions[i].row >= 0 && Positions[i].row < simGrid.getRowsNumber())
+                    && (Positions[i].column >= 0) && Positions[i].column < simGrid.getColumnsNumber()
+                    && simGrid.getMapCell(Positions[i].row, Positions[i].column) == Grid.EMPTY) {
+                tempPot = simGrid.getPotential(Positions[i].row, Positions[i].column)[id];
+                if (tempPot < minPot) {
+                    minPot = tempPot;
+                }
+                minCellPot[i] = minPot;
+            } else {
+                minCellPot[i] = Integer.MAX_VALUE;
+            }
+        }
+        
+        Position newPosition = new Position(row, column);
+        if (minPot == Integer.MAX_VALUE) {
+            return new Position(row, column);
+        }
+
+        LinkedList<Position> equalMinPos = new LinkedList<Position>();
+        for (int i = 0; i < 8; i++) {
+            if (minCellPot[i] == minPot) {
+                equalMinPos.add(Positions[i]);
+            }
+        }
+        newPosition = equalMinPos.get(random.nextInt(equalMinPos.size()));
+        return newPosition;
+
+    }
+     
+    
+    
+    
+    
 
     public void step() {
         peopleToProcess = new PriorityQueue<PersonPosition>();
